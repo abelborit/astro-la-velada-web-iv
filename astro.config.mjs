e90d1c5c681b96f1b5baf52e98102dde37c32718
Abel Borit Guitton <abelboritguitton@gmail.com>
2026-03-03 11:42:28 -0500

    [core] add basic configs of astro and typescript

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
+import path from "path";
+
+/* Vite espera rutas absolutas (desde la raíz del proyecto), no relativas como solo colocar "./src/components" */
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+// console.log({__dirname});
+// console.log("__dirname", __dirname + "src/components");
+// console.log("path.join", path.join(__dirname, "src/components"));
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  /* Si usas los alias en imports de Astro, agrega la configuración en "astro.config.mjs" */
+  /* https://vite.dev/config/ */
+  vite: {
+    /* https://vite.dev/config/shared-options#resolve-alias */
+    resolve: {
+      /* OPCIÓN 1: Esto concatena rutas sin separador, lo que puede causar problemas en Windows */
+      // alias: {
+      //   "@components": __dirname + "src/components",
+      //   "@layouts": __dirname + "src/layouts",
+      //   "@utils": __dirname + "src/utils",
+      // },
+
+      /* OPCIÓN 2: usar path.join para construir rutas correctamente */
+      alias: {
+        "@components": path.join(__dirname, "src/components"),
+        "@layouts": path.join(__dirname, "src/layouts"),
+        "@utils": path.join(__dirname, "src/utils"),
+      },
+    },
+  },
+});
